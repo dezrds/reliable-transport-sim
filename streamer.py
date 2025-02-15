5878a37b320826f646ab020427f1085e6bbfdb5c
dezrds <mhendejr30@gmail.com>
2025-02-14 21:17:41 -0600

    implemented reordering

--- streamer.py
+++ streamer.py
@@ -3,6 +3,9 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 
+import struct
+import time
+
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -14,27 +17,61 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
 
+        #reciever
+        self.expected_seq = 1         
+        self.rec_buffer = {}         
+
+
+        #sender
+        self.send_seq_num = 0
+
+
+
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
         len_bytes = len(data_bytes)
         bytes_sent = 0
-        packet_size = 1000
+        packet_size = 500
         chunk_begin = 0
 
         while bytes_sent < len_bytes:
-            self.socket.sendto(data_bytes[chunk_begin:chunk_begin+packet_size], (self.dst_ip, self.dst_port))
+            chunk = data_bytes[chunk_begin:chunk_begin+packet_size]
+            chunk_size = len(chunk)
+            self.send_seq_num += 1
+            packed_data = struct.pack(f"iI500s", self.send_seq_num, chunk_size, chunk)
+            self.socket.sendto(packed_data, (self.dst_ip, self.dst_port))
             chunk_begin += packet_size
             bytes_sent += packet_size
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
-        
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        last_time = time.time()
+
+        while True:
+            if self.expected_seq in self.rec_buffer:
+                payload_data = self.rec_buffer[self.expected_seq]
+                del self.rec_buffer[self.expected_seq]
+                self.expected_seq += 1
+                return payload_data
+            
+            if time.time() - last_time > 1.5:
+                return b''
+
+            incoming_packet, addr = self.socket.recvfrom(508)
+            seq_num, chunk_size = struct.unpack("iI", incoming_packet[:8])
+            if seq_num == -1:
+                self.final_seq = chunk_size
+            else:
+                payload_data = incoming_packet[8 : 8 + chunk_size]
+                if seq_num >= self.expected_seq:
+                    self.rec_buffer[seq_num] = payload_data
+
+            
+
+
+        return payload_data
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
